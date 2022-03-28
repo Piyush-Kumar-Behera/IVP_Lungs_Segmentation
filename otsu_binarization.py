da4a5200eabb9a4f3a48e89b23c7ebdbd7cd1026
Piyush-Kumar-Behera <48685838+Piyush-Kumar-Behera@users.noreply.github.com>
2022-03-28 23:06:59 +0530

    Corrected errors in algo

--- otsu_binarization.py
+++ otsu_binarization.py
@@ -11,18 +11,41 @@ def generate_hist(img):
     
     return hist
 
+def ctn(hist, s, e):
+    w = 0
+    for i in range(s, e):
+        w += hist[i]
+    return w
+
+def calc_mean(hist, s, e):
+    m = 0
+    w = ctn(hist, s, e)
+    for i in range(s, e):
+        m += hist[i] * i
+    
+    return m/float(w)
+
+def calc_var(hist, s, e):
+    v = 0
+    m = calc_mean(hist, s, e)
+    w = ctn(hist, s, e)
+    for i in range(s, e):
+        v += ((i - m) **2) * hist[i]
+    v /= w
+    return v
+
 def get_score(img, th, hist):
     w0 = np.sum(hist[:th])/np.sum(hist)
     w1 = np.sum(hist[th:])/np.sum(hist)
 
-    v0 = np.var(hist[:th])
-    v1 = np.var(hist[th:])
+    v0 = calc_var(hist, 0, th)
+    v1 = calc_var(hist, th, 256)
 
     return w0*v0 + w1*v1
 
 def get_threshold(img, hist):
     scores = []
-    for th in range(1,254):
+    for th in range(10,200):
         scores.append(get_score(img, th, hist))
     
     return np.argmin(scores) + 1
@@ -37,7 +60,7 @@ def otsu_thresholding(img):
         for j in range(n):
             if img[i][j] > thresh:
                 out[i][j] = 255
-    
+    print(thresh)
     return out
 
 if __name__ == '__main__':
